281ae988f5ba701fa297b827f8696b10cbeebed7
Lee Richardson <github@leerichardson.com>
2016-06-11 01:56:37 -0400

    Show fake led and audio patterns

--- SirenOfShame.Uwp.Web/Ts/show-off.component.ts
+++ SirenOfShame.Uwp.Web/Ts/show-off.component.ts
@@ -4,9 +4,45 @@ import {Routes, Router, Route, ROUTER_DIRECTIVES} from '@angular/router';
 @Component({
     template: `
 <h1>Show Off</h1>
-<router-outlet></router-outlet>
+<form class="form-horizontal">
+    <div class="form-group">
+        <label for="ledPattern" class="col-sm-2 control-label">LED Pattern</label>
+        <div class="col-sm-10">
+            <select id="ledPattern" class="form-control">
+                <option *ngFor="let pattern of ledPatterns">{{pattern}}</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="audioPattern" class="col-sm-2 control-label">Audio Pattern</label>
+        <div class="col-sm-10">
+            <select id="audioPattern" class="form-control">
+                <option *ngFor="let pattern of audioPatterns">{{pattern}}</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-primary">Play</button>
+        </div>
+    </div>
+</form>
 `
 })
 export class ShowOff {
-    
+    constructor() {
+        this.ledPatterns = [
+            'Led 1',
+            'Led 2',
+            'Led 3'
+        ];
+        this.audioPatterns = [
+            'Audio 1',
+            'Audio 2',
+            'Audio 3'
+        ];
+    }
+
+    public ledPatterns:string[];
+    public audioPatterns:string[];
 }
