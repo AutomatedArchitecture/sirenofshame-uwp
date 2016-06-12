327859592d7ec17d8f1bc22112c7536bbca8ddf9
Lee Richardson <github@leerichardson.com>
2016-06-12 16:16:26 -0400

    Added a Watcher PCL and unit test project

---
+++ SirenOfShame.Uwp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using SirenOfShame.Uwp.Watcher;
+
+namespace SirenOfShame.Uwp.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, Calculator.Add(1, 1));
+        }
+    }
+}
---
+++ SirenOfShame.Uwp.Watcher/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SirenOfShame.Uwp.Watcher
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
