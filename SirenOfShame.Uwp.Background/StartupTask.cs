62ebae060812ea9f5e48452a0d6e7fb865a2438a
Lee Richardson <github@leerichardson.com>
2016-06-18 00:08:18 -0400

    Ability to send and receive messages to/from the server via web sockets

--- SirenOfShame.Uwp.Background/StartupTask.cs
+++ SirenOfShame.Uwp.Background/StartupTask.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using Windows.ApplicationModel.Background;
+using Windows.Web.Http.Filters;
 using IotWeb.Common.Http;
 using IotWeb.Common.Util;
 using SirenOfShame.Uwp.Background.Services;
@@ -8,6 +9,28 @@ using IotWeb.Server;
 
 namespace SirenOfShame.Uwp.Background
 {
+    /// <summary>
+    /// Simple 'echo' web socket server
+    /// </summary>
+    class WebSocketHandler : IWebSocketRequestHandler
+    {
+
+        public bool WillAcceptRequest(string uri, string protocol)
+        {
+            return (uri.Length == 0) && (protocol == "echo");
+        }
+
+        public void Connected(WebSocket socket)
+        {
+            socket.DataReceived += OnDataReceived;
+        }
+
+        void OnDataReceived(WebSocket socket, string frame)
+        {
+            socket.Send(frame);
+        }
+    }
+
     public sealed class StartupTask : IBackgroundTask
     {
         // ReSharper disable once NotAccessedField.Local
@@ -18,6 +41,11 @@ namespace SirenOfShame.Uwp.Background
         {
             _backgroundTaskDeferral = taskInstance.GetDeferral();
             _httpServer = new HttpServer(8001);
+            _httpServer.AddWebSocketRequestHandler(
+                "/sockets/",
+                new WebSocketHandler()
+                );
+            ;
             _httpServer.AddHttpRequestHandler(
                 "/",
                 new HttpResourceHandler(typeof(StartupTask).GetTypeInfo().Assembly,
--- SirenOfShame.Uwp.Web/Ts/home.component.ts
+++ SirenOfShame.Uwp.Web/Ts/home.component.ts
@@ -1,9 +1,46 @@
-﻿import {Component} from '@angular/core';
+﻿import {Component, OnInit} from '@angular/core';
 
 @Component({
     template: `
     <h1>Siren of Shame</h1>
+    <h2>Messages</h2>
+    <div>{{messages}}</div>
+    <h2>Errors</h2>
+    <div>{{errors}}</div>
+    <h2>Send</h2>
+    <input type="text" [(ngModel)]="message" />
+    <button type="submit" (click)="onButtonClick()">Send</button>
 `
 })
-export class Home {
+export class Home implements OnInit {
+    private ws;
+    public message: string;
+    public messages: string;
+    public errors: string;
+
+    public onButtonClick() {
+        this.ws.send(this.message);
+    }
+
+    public ngOnInit() {
+        this.messages = '';
+        if ("WebSocket" in window) {
+            var wsUrl = "ws://" + location.hostname + (location.port ? ':' + location.port : '') + "/sockets/";
+            var connection = new WebSocket(wsUrl, ['echo']);
+            connection.onopen = () => {
+                this.ws = connection;
+                this.ws.onmessage = (e) => {
+                    this.messages += e.data;
+                };
+                this.errors = "Connection established";
+                this.ws.send("Hello world!");
+            };
+            connection.onerror = (error) => {
+                this.errors = "Could not connect to websocket. " + error;
+            };
+        }
+        else {
+            this.errors = "This browser does not support websockets!";
+        }
+    }
 }
