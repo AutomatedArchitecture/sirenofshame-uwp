2c1fafc15e6fc349861e8106f6db80f3db651c90
Lee Richardson <github@leerichardson.com>
2016-07-27 00:02:42 -0400

    A settings page

--- SirenOfShame.Uwp.Web/Ts/app.routes.ts
+++ SirenOfShame.Uwp.Web/Ts/app.routes.ts
@@ -2,13 +2,15 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import {ShowOff} from './show-off.component'
 import {Home} from './home.component'
 import {Server} from './server.component'
+import {Settings} from './settings.component'
 
 export const routes: RouterConfig = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: Home },
     { path: 'showoff', component: ShowOff },
     { path: 'server', component: Server },
-    { path: 'server/:id', component: Server }
+    { path: 'server/:id', component: Server },
+    { path: 'settings', component: Settings }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
---
+++ SirenOfShame.Uwp.Web/Ts/settings.component.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from '@angular/core';
+
+@Component({
+    template: `
+<h1>Settings</h1>
+`
+})
+export class Settings {
+    
+}
