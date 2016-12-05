1cf6d520a1fc19386d338aae1f1a2e14bc0861d4
Lee Richardson <github@leerichardson.com>
2016-12-04 22:25:09 -0500

    Displaying fake build definitions

--- SirenOfShame.Uwp.Ui/MainPage.xaml.cs
+++ SirenOfShame.Uwp.Ui/MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
+using SirenOfShame.Uwp.Ui.Models;
 using SirenOfShame.Uwp.Ui.Services;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -21,6 +23,25 @@ namespace SirenOfShame.Uwp.Ui
             InitializeComponent();
             _connection = ServiceContainer.Resolve<MessageRelayService>();
             _connection.OnMessageReceived += ConnectionOnMessageReceived;
+            LoadInitialData();
+        }
+
+        private void LoadInitialData()
+        {
+            BuildDefinitionsSource.Source = new List<BuildDefinition>
+            {
+                MakeBuildDefinition(1, "Build Def #1"),
+                MakeBuildDefinition(2, "Build Def #2"),
+                MakeBuildDefinition(3, "Build Def #3"),
+                MakeBuildDefinition(4, "Build Def #4"),
+                MakeBuildDefinition(5, "Build Def #5"),
+                MakeBuildDefinition(6, "Build Def #6"),
+            };
+        }
+
+        private static BuildDefinition MakeBuildDefinition(int id, string title)
+        {
+            return new BuildDefinition { BuildDefinitionId = id, Title = title };
         }
 
         private async void ConnectionOnMessageReceived(ValueSet valueSet)
---
+++ SirenOfShame.Uwp.Ui/Models/BuildDefinition.cs
@@ -0,0 +1,8 @@
+﻿namespace SirenOfShame.Uwp.Ui.Models
+{
+    public class BuildDefinition
+    {
+        public int BuildDefinitionId { get; set; }
+        public string Title { get; set; }
+    }
+}
