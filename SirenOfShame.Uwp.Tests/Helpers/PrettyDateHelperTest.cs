831fda98de0e6a72700408ac6043f3c8b74c7325
Lee Richardson <github@leerichardson.com>
2016-12-31 18:20:23 -0500

    pretty date testing

---
+++ SirenOfShame.Uwp.Tests/Helpers/PrettyDateHelperTest.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using NUnit.Framework;
+using SirenOfShame.Uwp.Watcher.Helpers;
+
+namespace SirenOfShame.Test.Unit.Helpers
+{
+    [TestFixture]
+    public class PrettyDateHelperTest
+    {
+        [Test]
+        public void WhenIsNow()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            Assert.AreEqual("just now", d.PrettyDate(now));
+        }
+        
+        [Test]
+        public void FiftyNineSecondsAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 1, 0, 0, 59, 999);
+            Assert.AreEqual("just now", d.PrettyDate(now));
+        }
+        
+        [Test]
+        public void OneMinuteAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 1, 0, 1, 0, 0);
+            Assert.AreEqual("1 minute ago", d.PrettyDate(now));
+        }
+        
+        [Test]
+        public void OneMinuteFiftyNineSecondsAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 1, 0, 1, 59, 999);
+            Assert.AreEqual("1 minute ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void TwoMinutesAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 1, 0, 2, 0, 0);
+            Assert.AreEqual("2 minutes ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void FiftyNineMinutesAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 1, 0, 59, 59, 999);
+            Assert.AreEqual("59 minutes ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void OneHourAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 1, 1, 0, 0, 0);
+            Assert.AreEqual("1 hour ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void OneHourFiftyNineMinutesAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 1, 1, 59, 59, 999);
+            Assert.AreEqual("1 hour ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void TwoHoursAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 1, 2, 0, 0, 0);
+            Assert.AreEqual("2 hours ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void TwentyThreeHoursAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 1, 23, 59, 59, 999);
+            Assert.AreEqual("23 hours ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void OneDayAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 2, 0, 0, 0, 0);
+            Assert.AreEqual("yesterday", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void OneDayTwentyThreeHoursAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 2, 23, 59, 59, 999);
+            Assert.AreEqual("yesterday", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void TwoDaysAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 3, 0, 0, 0, 0);
+            Assert.AreEqual("2 days ago", d.PrettyDate(now));
+        }
+        
+        [Test]
+        public void SixDaysAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 7, 23, 59, 59, 999);
+            Assert.AreEqual("6 days ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void SevenDaysAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 8, 0, 0, 0, 0);
+            Assert.AreEqual("last week", d.PrettyDate(now));
+        }
+        
+        [Test]
+        public void ThirteenDaysAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 14, 23, 59, 59, 999);
+            Assert.AreEqual("last week", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void FourteenDaysAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 15, 0, 0, 0, 0);
+            Assert.AreEqual("2 weeks ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void ThreeWeeksAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 1, 28, 23, 59, 59, 999);
+            Assert.AreEqual("3 weeks ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void FourWeeksAgo()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 2, 4, 23, 59, 59, 999);
+            Assert.AreEqual("4 weeks ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void OneMonthAgoMin()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 2, 5, 0, 0, 0, 0);
+            Assert.AreEqual("1 month ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void OneMonthAgoMax()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 3, 1, 23, 59, 59, 999);
+            Assert.AreEqual("1 month ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void TwoMonthsAgoMin()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 3, 3, 0, 0, 0, 0);
+            Assert.AreEqual("2 months ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void TwoMonthsAgoMax()
+        {
+            var d = new DateTime(2010, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 3, 31, 23, 59, 59, 999);
+            Assert.AreEqual("2 months ago", d.PrettyDate(now));
+        }
+
+        [Test]
+        public void OneYearAgo()
+        {
+            var d = new DateTime(2009, 1, 1, 0, 0, 0, 0);
+            var now = new DateTime(2010, 2, 5, 0, 0, 0, 0);
+            Assert.AreEqual(d.ToString("d"), d.PrettyDate(now));
+        }
+
+    }
+}
---
+++ SirenOfShame.Uwp.Tests/Helpers/StringHelpersTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using SirenOfShame.Lib.Helpers;
+
+namespace SirenOfShame.Test.Unit.Helpers
+{
+    [TestFixture]
+    public class StringHelpersTest
+    {
+        [Test]
+        public void RemoveHttpPrefix_Null_Null()
+        {
+            Assert.IsNull(StringHelpers.RemoveUrlPrefix(null));
+        }
+        
+        [Test]
+        public void RemoveUrlPrefix_UnderLength_ReturnsFullString()
+        {
+            Assert.AreEqual("server", StringHelpers.RemoveUrlPrefix("server"));
+        }
+        
+        [Test]
+        public void RemoveUrlPrefix_TypicalUrl_RemovesHttp()
+        {
+            Assert.AreEqual("google.com", StringHelpers.RemoveUrlPrefix("http://google.com"));
+        }
+    }
+}
