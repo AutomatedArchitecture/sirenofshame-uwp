45070303cf9f68010c3431f9eac9da2d2765cc4a
Lee Richardson <github@leerichardson.com>
2017-02-22 00:45:24 -0500

    Fixed authentication when retrieving Jenkins builds
    Issue #9

--- SirenOfShame.Uwp.Server/Commands/GetBuildDefinitionsCommand.cs
+++ SirenOfShame.Uwp.Server/Commands/GetBuildDefinitionsCommand.cs
@@ -1,13 +1,11 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using SirenOfShame.Uwp.Server.Models;
 using SirenOfShame.Uwp.Watcher;
 using SirenOfShame.Uwp.Watcher.Settings;
 
 namespace SirenOfShame.Uwp.Server.Commands
 {
-
     public class GetProjectsRequest : RequestBase
     {
         public CiServer CiServer { get; set; }
@@ -22,14 +20,12 @@ namespace SirenOfShame.Uwp.Server.Commands
         public string Password { get; set; }
     }
 
-    internal class GetBuildDefinitionsCommand : CommandBase
+    internal class GetBuildDefinitionsCommand : CommandBase<GetProjectsRequest>
     {
         public override string CommandName => "getBuildDefinitions";
 
-        public override async Task<SocketResult> Invoke(string frame)
+        protected override async Task<SocketResult> Invoke(GetProjectsRequest getProjectsRequest)
         {
-            var getProjectsRequest = JsonConvert.DeserializeObject<GetProjectsRequest>(frame);
-
             var ciServer = getProjectsRequest.CiServer;
             var ciEntryPoint = SirenOfShameSettings.CiEntryPoints.First(i => i.Name == ciServer.Name);
 
--- SirenOfShame.Uwp.Web/Ts/models/ciEntryPointSetting.ts
+++ SirenOfShame.Uwp.Web/Ts/models/ciEntryPointSetting.ts
@@ -4,6 +4,8 @@ export class CiEntryPointSetting {
     public id: number;
     public name: string;
     public url: string;
+    public username: string;
+    public password: string;
     public projects: MyBuildDefinition[];
     public buildDefinitionSettings: MyBuildDefinition[];
 }
