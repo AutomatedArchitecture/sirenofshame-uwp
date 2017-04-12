e8f660edfe49c394a8fe1371e82ca87f650d9215
Lee Richardson <github@leerichardson.com>
2017-04-12 19:58:20 -0400

    Ability to click a user and navigate

--- SirenOfShame.Uwp.Ui/Controls/Leaders.xaml.cs
+++ SirenOfShame.Uwp.Ui/Controls/Leaders.xaml.cs
@@ -1,27 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using SirenOfShame.Uwp.Ui.Views;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace SirenOfShame.Uwp.Ui.Controls
 {
-    public sealed partial class Leaders : UserControl
+    public sealed partial class Leaders
     {
         public Leaders()
         {
             this.InitializeComponent();
+        }
+
+        private void ListViewBase_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            var page = FindParent<Page>();
+            page.Frame.Navigate(typeof(ViewUser));
+        }
+
+        private T FindParent<T>() where T : FrameworkElement
+        {
+            return FindParent<T>(this);
+        }
+
+        private T FindParent<T>(FrameworkElement element) where T : FrameworkElement
+        {
+            if (element == null) return default(T);
+            var parent = element.Parent as FrameworkElement;
+            var parentAsT = parent as T;
+            if (parentAsT != null)
+                return parentAsT;
+            return FindParent<T>(parent);
         }
     }
 }
