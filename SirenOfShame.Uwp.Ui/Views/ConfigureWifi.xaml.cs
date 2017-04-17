ffdddf421f0f05a6cbb336efe0b4c989b0315923
Lee Richardson <github@leerichardson.com>
2017-04-17 09:15:04 -0400

    PageBase
    Issue #10

--- SirenOfShame.Uwp.Ui/Views/ConfigureWifi.xaml.cs
+++ SirenOfShame.Uwp.Ui/Views/ConfigureWifi.xaml.cs
@@ -1,30 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace SirenOfShame.Uwp.Ui.Views
+﻿namespace SirenOfShame.Uwp.Ui.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ConfigureWifi : Page
+    public sealed partial class ConfigureWifi
     {
         public ConfigureWifi()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
     }
 }
--- SirenOfShame.Uwp.Ui/Views/ViewUser.xaml.cs
+++ SirenOfShame.Uwp.Ui/Views/ViewUser.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Navigation;
 using SirenOfShame.Uwp.Ui.Models;
 using SirenOfShame.Uwp.Ui.ViewModels;
@@ -22,11 +10,11 @@ namespace SirenOfShame.Uwp.Ui.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ViewUser : Page
+    public sealed partial class ViewUser
     {
         public ViewUser()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
