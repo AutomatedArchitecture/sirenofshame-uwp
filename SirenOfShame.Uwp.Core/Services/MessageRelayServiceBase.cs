87b6e7b0a7ffa3ce9b3a6c19836676a3ae746c98
Lee Richardson <github@leerichardson.com>
2018-01-16 23:07:48 -0500

    Wait way longer to find a MessageRelay since the app may be starting up for the very 1st time

--- SirenOfShame.Uwp.Core/Services/MessageRelayServiceBase.cs
+++ SirenOfShame.Uwp.Core/Services/MessageRelayServiceBase.cs
@@ -15,16 +15,23 @@ namespace SirenOfShame.Uwp.Core.Services
             var policyResult = await Policy.HandleResult<string>(i => i == null)
                 .WaitAndRetryAsync(new[]
                     {
-                        TimeSpan.FromSeconds(1),
-                        TimeSpan.FromSeconds(4),
+                        TimeSpan.FromSeconds(5),
                         TimeSpan.FromSeconds(15),
                         TimeSpan.FromSeconds(20),
-                        TimeSpan.FromSeconds(20),
-                        TimeSpan.FromSeconds(20),
+                        TimeSpan.FromSeconds(30),
+                        TimeSpan.FromSeconds(30),
+                        TimeSpan.FromSeconds(30),
+                        TimeSpan.FromSeconds(30),
+                        TimeSpan.FromSeconds(30),
+                        TimeSpan.FromSeconds(30),
+                        TimeSpan.FromSeconds(30),
+                        TimeSpan.FromSeconds(30),
+                        TimeSpan.FromSeconds(30),
+                        TimeSpan.FromSeconds(30),
                     },
                     (exception, timeSpan, retryCount, context) =>
                     {
-                        _log.Warn("Unable to find Message Relay app.  Attempt #" + retryCount);
+                        _log.Warn($"Can't find MessageRelay app.  Attempt #{retryCount}. Trying again in {timeSpan.TotalSeconds} seconds.");
                     })
                 .ExecuteAndCaptureAsync(TryFindMessageRelayAppPackageFamilyName);
             return policyResult.Result;
